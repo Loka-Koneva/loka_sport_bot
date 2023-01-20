0c742da68835b85a4863bdf72b644c4f80c52a16
Uliana Koneva <loka.koneva@gmail.com>
2023-01-21 01:14:37 +0500

    Добавлена обработка команды \start.

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import telebot
-import config
+import config_dev as config
 
 bot = telebot.TeleBot(config.TOKEN)
+
+
+@bot.message_handler(commands=['start'])
+def welcom_message(message):
+    with open('./static/my_photo.jpg', 'rb') as photo:
+        bot.send_photo(message.chat.id, photo)
+    with open('./static/welcom_message.txt', 'r') as text:
+        bot.send_message(message.chat.id, text.read().format(message.from_user.first_name))
 
 @bot.message_handler(content_types=['text'])
 def lalala(message):
     bot.send_message(message.chat.id, message.text)
+
 
 bot.polling(none_stop=True)
